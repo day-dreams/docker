528aefb796e8cc0b2d83acf709a68796fadfeec3
zhangnan <xyz.zhangnan@gmail.com>
2019-01-31 21:01:34 +0800

    [fix] fix mongodb start cmds

--- mongo/start.sh
+++ mongo/start.sh
@@ -2,10 +2,20 @@
 
 docker pull mongo
 
-docker continer rm --force my-mongodb-v0.1
+docker container rm --force my-mongodb-v0.1
 
 docker run --name my-mongodb-v0.1 -p 27017:27017 -v $PWD/db:/data/db -d mongo --auth
 
 
-# 本机开启mongo shell
-# 创建好db、user后，开启auth选项即可
+# 初次启动，需要删掉--auth选项，进入mongo shell后创建好用户，下次启动再开启--auth选项
+
+
+#use dbname 
+#db.createUser({
+#... user:"username",
+#... pwd:"helloworld",
+#... roles:[{role:"readWrite",db:"dbname"}],
+#... passwordDigestor:"server",
+#... }
+#... )
+
